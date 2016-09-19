d4374f42f0468657d697b1299d9a454a22385ece
Simon Morand <simon.morand@gmail.com>
2016-09-19 22:52:17 +0200

    adds aliases

--- PoshDog/PoshDog.psm1
+++ PoshDog/PoshDog.psm1
@@ -14,10 +14,14 @@
             Write-Error -Message "Failed to import file $($import.fullname): $_"
         }
     }
+Set-Alias -Name Mute-DDMonitor -Value Suspend-DDMonitor -Description 'Mutes Datadog monitors.' -Force
+Set-Alias -Name Unmute-DDMonitor -Value Resume-DDMonitor -Description 'Unmutes Datadog monitors.' -Force
+Set-Alias -Name Mute-DDHost -Value Suspend-DDHost -Description 'Mutes Datadog hosts.' -Force
+Set-Alias -Name Unmute-DDHost -Value Resume-DDHost -Description 'Unmutes Datadog hosts.' -Force
 
 # Here I might...
     # Read in or create an initial config file and variable
     # Export Public functions ($Public.BaseName) for WIP modules
     # Set variables visible to the module and its functions only
 
-Export-ModuleMember -Function $Public.Basename
+Export-ModuleMember -Function $Public.Basename -Alias *
